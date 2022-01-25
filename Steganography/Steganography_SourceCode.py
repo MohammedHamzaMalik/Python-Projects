7382ad5b3b2442f4ca161181398733dac2b419bb
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2022-01-25 22:56:27 +0530

    explanation added

--- Steganography/Steganography_SourceCode.py
+++ Steganography/Steganography_SourceCode.py
@@ -94,6 +94,27 @@ def main_decryption(img, strvar):
             strvar.set(data)
 
 
+'''
+Explanation:
+
+
+1. In these functions, we will get the pixel data from the images, and then alter that data to keep the appearance of the image the same but encode the text that we want to hide in the data.
+
+2. Image class is used to make an image file get recognized by Python. Its methods are:
+
+The .open() method is used to open an Image file in a flag mode.
+The .getdata() method is used to get the pixel data from the image.
+The .copy() method is used to copy an image and assign it to another variable.
+The .save(<filename>, <filetype>) is used to save a file with the file name ‘filename’ and type ‘filetype’.
+3. Presence of the yield keyword tells us that the generate_data() function is a generator.
+
+4. The yield keyword is used to return values from the generator without destroying the local variables or by ending the generator.
+
+5. iter() function is used to return the values provided to it as arguments in the form of an iterable data type.
+
+6. __next__() function is used to get the next values from the iterator it is applied to.
+'''
+
 # Creating the button functions
 def encode_image():
     encode_wn = Toplevel(root)
@@ -150,6 +171,28 @@ def decode_image():
     text_entry.place(x=15, y=160, height=100)
 
 
+'''
+
+Explanation:
+
+1. The Toplevel class is useful to create a toplevel widget, or a child window associated with the root window.
+
+You need to set the same attributes and methods to this class as is done during the creation of the root window.
+2. The Entry class is to create an entry field to the window where the user can input data. The parameters and methods of this class that are undiscussed are:
+
+The state parameter is to define the actions that the user can do inside the Entry widget.
+The default, normal, allows the user to enter data as well.
+The readonly state only allows the user to read and copy the data.
+The disabled state only allows the user to read the data only.
+The text parameter is for setting the variable that will manipulate the data in the widget or to the initial text inside the widget.
+The .get() method of this class is to get the value inside the widget that this class creates.
+The StringVar class is for manipulating the text inside the Label, Entry and many other widgets.
+
+The .set() method of this class changes the value of this widget to the argument it is provided.
+
+'''
+
+
 # Initializing the window
 root = Tk()
 root.title('Image Steganography')
@@ -169,3 +212,31 @@ Button(root, text='Decode', width=25, font=('Times New Roman', 13), bg='SteelBlu
 # Finalizing the window
 root.update()
 root.mainloop()
+
+
+'''
+Explanation:
+
+We will assign the Tk() class to the root variable to initialize the window. The methods and attributes that need to be set during initializing are:
+
+The .title() method is used to give a title to the window.
+The .geometry() method is used to define the initial dimensions of the window in pixels.
+The .resizable() method specifies whether the user will be allowed to resize the window or not. It takes only truth and falsy values in the form of (width, height).
+The .config() method is used to configure other attributes of the window, such as the bg attribute which denotes the colour of the background of the window.
+The .update() and .mainloop() methods put the window in a loop to prevent it from closing until told otherwise.
+Note:
+These two lines will be the last lines in a GUI script that the Python interpreter will run in it.
+The Label class is used to create a Label on the window that displays static text on the window. Its parameters that need to be set during assignment are:
+
+The master parameter, the positional argument root here, is the parent widget this widget is associated with.
+The text parameter refers to the text that will be displayed on the label.
+The font parameter defines the font family, size and effects that will be applied to the text displayed on the label.
+The wraplength parameter specifies the number of pixels after which the text has to be moved to the next line.
+The Button class is used to add a button to the window that runs a function when it is pressed. Its parameters are:
+
+The command parameter is used to define the function that will run when the button is pressed. You will not need the lambda keyword if the function to run does not need any arguments.
+The .place() method is used to place a widget on its parent widget as though the parent is a Cartesian Plane, with the Northwest corner being the origin of that plane.
+
+The x,y parameters define the horizontal and vertical offsets of the widget, respectively.
+The relx,rely parameters define the horizontal and vertical offsets as a decimal number between 0.0 and 1.0
+'''
